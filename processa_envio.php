6b02581cec0367bcb424a4aa99eaeb72f95d0693
yulpaiva <yulbrynner73@gmail.com>
2023-08-09 14:05:53 -0300

    commit

--- processa_envio.php
+++ processa_envio.php
@@ -112,48 +112,6 @@
         $mensagem->status['descricao_status'] = 'Não foi possivel enviar este e-mail! Por favor tente novamente mais tarde.';
     }
 
-
-
-/* Template
-
-    $mail = new PHPMailer(true);
-	try {
-        //Server settings
-        $mail->SMTPDebug = 2;                      //Enable verbose debug output
-        $mail->isSMTP();                                            //Send using SMTP
-        $mail->Host       = 'smtp-mail.outlook.com';                     //Set the SMTP server to send through
-        $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-        $mail->Username   = 'xxyulxx@outlook.com';                     //SMTP username
-        $mail->Password   = 'Pain2014';                               //SMTP password
-        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-        $mail->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
-
-        //Recipients
-        $mail->setFrom('xxyulxx@outlook.com', 'Yul Remetente');         //remetente
-        $mail->addAddress('xxyulxx@outlook.com', 'Yul Destinatário');     //Add a recipient
-        $mail->addAddress('yulbrynner73@gmail.com');               //Name is optional
-        //$mail->addReplyTo('info@example.com', 'Information');
-        //$mail->addCC('cc@example.com');
-        //$mail->addBCC('bcc@example.com');
-
-        //Attachments
-        //$mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
-        //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
-
-        //Content
-        $mail->isHTML(true);                                  //Set email format to HTML
-        $mail->Subject = 'Oi eu sou o Assunto';
-        $mail->Body    = 'Oi .Eu sou o conteudo do <strong> e-mail.</strong>';
-        $mail->AltBody = 'Oi .Eu sou o conteudo do e-mail.';
-
-        $mail->send();
-        echo 'Message has been sent';
-    } catch (Exception $e) {
-        echo "Não foi possivel enviar este e-mail! Por favor tente novamente mais tarde.";
-        echo 'Detalhes do erro: ' . $mail->ErrorInfo;
-    }
-
-*/
     
 ?>
 
